0ec6689521051a21256839de5b4c30ba2922aa93
James <jamezbrown100@gmail.com>
2016-10-11 13:43:50 -0500

    Setup tableView vc

---
+++ Scheduler/RemindersViewController.swift
@@ -0,0 +1,37 @@
+//
+//  RemindersViewController.swift
+//  Scheduler
+//
+//  Created by James Brown on 10/11/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class RemindersViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "ReminderCell") {
+            return cell
+        }
+        
+        return UITableViewCell()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+ }
