f227d9cbed5af59c5adae707e7696473ee6021a6
James <jamezbrown100@gmail.com>
2016-10-11 18:34:54 -0500

    Correctly format date

--- AddTableViewController.swift
+++ AddTableViewController.swift
@@ -12,16 +12,14 @@ class AddTableViewController: UITableViewController {
 
     @IBOutlet weak var entryDateCell: UITableViewCell!
     
-    var date: Date!
+    var date = Date()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationItem.hidesBackButton = true
         
-        date = Date()
-        
-        entryDateCell.textLabel?.text = "\(date!)"
+        displayDate()
         
         NotificationCenter.default.addObserver(self, selector: #selector(updateEntryDate(notification:)), name: NSNotification.Name(rawValue: "entry_date"), object: nil)
     }
@@ -47,8 +45,16 @@ class AddTableViewController: UITableViewController {
     func updateEntryDate(notification: Notification) {
         if let date = notification.object as? Date {
             self.date = date
-            entryDateCell.textLabel?.text = "\(date)"
+            displayDate()
         }
+    }
+    
+    func displayDate() {
+        let formatter = DateFormatter()
+        formatter.dateStyle = DateFormatter.Style.long
+        formatter.timeStyle = .short
+        
+        entryDateCell.textLabel?.text = "\(formatter.string(from: date))"
     }
 
 }
