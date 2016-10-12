9334615cd3b12f75ef163d62259baddcf9c4b9b7
James <jamezbrown100@gmail.com>
2016-10-12 06:45:59 -0500

    Send notification for repeat interval

--- RepeatTableViewController.swift
+++ RepeatTableViewController.swift
@@ -12,17 +12,15 @@ class RepeatTableViewController: UITableViewController {
     
     var currentSelectedIndex = IndexPath(row: 0, section: 0)
 
+    
+    // MARK: methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.hidesBackButton = true
     }
     
-    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
-        _ = navigationController?.popViewController(animated: true)
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+      override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let cell = tableView.cellForRow(at: indexPath)
         cell?.accessoryType = .checkmark
         
@@ -30,6 +28,19 @@ class RepeatTableViewController: UITableViewController {
         oldCell?.accessoryType = .none
         
         currentSelectedIndex = indexPath
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        let cell = tableView.cellForRow(at: currentSelectedIndex)
+        if let text = cell?.textLabel?.text {
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "repeat_interval"), object: text)
+        }
+    }
+
+    
+    // MARK: IBActions
+    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+        _ = navigationController?.popViewController(animated: true)
     }
 
 }
