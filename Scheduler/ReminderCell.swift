71277b245f45972c50c54a0e94fa8aa8b8cd0326
James <jamezbrown100@gmail.com>
2016-10-12 17:35:53 -0500

    Create reminder cell class

---
+++ Scheduler/ReminderCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReminderCell.swift
+//  Scheduler
+//
+//  Created by James Brown on 10/12/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class ReminderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
