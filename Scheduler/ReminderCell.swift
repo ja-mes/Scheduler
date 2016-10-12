350670e645a944a9d6ff2257a5c62a15db1e5157
James <jamezbrown100@gmail.com>
2016-10-12 17:39:52 -0500

    Create outlets

--- Scheduler/ReminderCell.swift
+++ Scheduler/ReminderCell.swift
@@ -10,15 +10,14 @@ import UIKit
 
 class ReminderCell: UITableViewCell {
 
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var dateLbl: UILabel!
+    @IBOutlet weak var recipientLbl: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
