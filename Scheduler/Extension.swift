58c0fcd10851998809fe42bbf4dc22c2b860d60f
James <jamezbrown100@gmail.com>
2016-10-20 12:55:45 -0500

    Add extension file with method to truncate string

---
+++ Scheduler/Extension.swift
@@ -0,0 +1,27 @@
+//
+//  Extension.swift
+//  Scheduler
+//
+//  Created by James Brown on 10/20/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    /**
+     Truncates the string to the specified length number of characters and appends an optional trailing string if longer.
+     
+     - Parameter length: A `String`.
+     - Parameter trailing: A `String` that will be appended after the truncation.
+     
+     - Returns: A `String` object.
+     */
+    func truncate(length: Int, trailing: String = "…") -> String {
+        if self.characters.count > length {
+            return String(self.characters.prefix(length)) + trailing
+        } else {
+            return self
+        }
+    }
+}
