4cbcfb06515f1496fb2352aaae73814ec5f1c609
James <jamezbrown100@gmail.com>
2016-10-20 13:00:14 -0500

    Make sure recipient label can't expand over the past due stackview

--- Scheduler/RemindersViewController.swift
+++ Scheduler/RemindersViewController.swift
@@ -175,8 +175,7 @@ class RemindersViewController: UIViewController, UITableViewDataSource, UITableV
     
     func configureCell(cell: ReminderCell, indexPath: IndexPath) {
         let reminder = controller.object(at: indexPath)
-        cell.recipientLbl.text = "awesomeandwonderfulverylongemailaddresspleaserecheck@gmail.com"
-        
+        cell.recipientLbl.text = "awesomeandwonderfulverylongemailaddresspleaserecheck@gmail.com"        
         if reminder.type == "email" {
             cell.icon.image = #imageLiteral(resourceName: "email_icon")
         } else {
