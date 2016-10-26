24399637768a0fe7c54b5627d3fc9f4341b3f770
James <jamezbrown100@gmail.com>
2016-10-26 15:44:34 -0500

    Create manager class

---
+++ Scheduler/Manager.swift
@@ -0,0 +1,13 @@
+//
+//  Manager.swift
+//  Scheduler
+//
+//  Created by James Brown on 10/26/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import Foundation
+
+class Manager {
+    
+}
