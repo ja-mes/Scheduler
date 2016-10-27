5fdca65dfad38acaac5c56801ebf62afca392a1f
James <jamezbrown100@gmail.com>
2016-10-27 16:06:24 -0500

    Don't throw error sent in

--- Scheduler/Reminder+CoreDataClass.swift
+++ Scheduler/Reminder+CoreDataClass.swift
@@ -22,7 +22,7 @@ public class Reminder: NSManagedObject {
         } catch ValidationError.Invalid {
             
         } catch {
-            fatalError(error)
+            fatalError("Validation failed")
         }
         
         
