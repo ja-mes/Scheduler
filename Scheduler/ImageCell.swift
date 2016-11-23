0411d443fced84dc90de3bcda0dde8f35e448165
James <jamezbrown100@gmail.com>
2016-11-23 13:11:01 -0600

    Add image cell class

---
+++ Scheduler/ImageCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCell.swift
+//  Scheduler
+//
+//  Created by James Brown on 11/23/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+    @IBOutlet weak var image: UIImageView!
+}
