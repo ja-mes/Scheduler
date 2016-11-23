62edffde7fb75f19cdb5728af2de8fd9425535fc
James <jamezbrown100@gmail.com>
2016-11-23 12:50:16 -0600

    Setup collection view

---
+++ Scheduler/AttachImagesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AttachImagesViewController.swift
+//  Scheduler
+//
+//  Created by James Brown on 11/23/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class AttachImagesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
